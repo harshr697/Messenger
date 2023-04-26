d9c659d64069459d6bbeca663ff92b1165b928b0
Harsh Raghvani <harshraghvani@Harshs-MacBook-Air.local>
2023-04-26 23:55:40 +0530

    Getting Started

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Harsh Raghvani on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
